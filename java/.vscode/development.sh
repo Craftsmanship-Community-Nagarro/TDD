fe6bf8bec3c9c5d2b784e762a48e0c3bcc911a8d
Marco Tulio Ávila Cerón <marco.avila@nagarro.com>
2025-07-02 11:19:45 +0200

    none allow .vscode

---
+++ java/.vscode/development.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script sets up a development environment by displaying some useful information.
+echo "Setting up development environment..."
+
+# Display current directory
+echo "Current directory: $(pwd)"
+
+# Diplay current user
+echo "Current user: $(whoami)"
+
+# Display current date and time
+echo "Current date and time: $(date)"
+
+# Good bye message
+echo "End of development setup. You can now start coding!"
